296f14ad075171971ccb13c35e2e77e499382e65
Aaron <troublor@live.com>
2020-12-07 00:29:49 +0800

    prepare experiment

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -103,12 +103,13 @@ var populateAccount = function () {
 
 $( document ).ready(function() {
   // set the web3 provider if not present
-  if (typeof web3 !== 'undefined') {
+  if (typeof web33 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
+    window.ethereum.enable();
     window.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
 
   Lottery.setProvider(web3.currentProvider);
